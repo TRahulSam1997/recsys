1b183860794e6a86cd5f19733989ed0665fe00a6
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-03-06 23:53:35 +1100

    user filtering update

--- main.py
+++ main.py
@@ -14,7 +14,24 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
 articles_df = pd.read_csv('./input/shared_articles.csv')
-articles_df = articles_df[articles_df['eventType'] == ['CONTENT SHARED']]
+articles_df = articles_df[articles_df['eventType'] == 'CONTENT SHARED']
 articles_df.head(5)
 
-print(articles_df)
+interactions_df = pd.read_csv('./input/users_interactions.csv')
+interactions_df.head(10)
+
+event_type_strength = {
+    'VIEW': 1.0,
+    'LIKE': 2.0,
+    'BOOKMARK': 2.5,
+    'FOLLOW': 3.0,
+    'COMMENT CREATED': 4.0,
+}
+
+interactions_df['eventStrength'] = interactions_df['eventType'].apply(lambda x: event_type_strength[x])
+
+users_interactions_count_df = interactions_df.groupby(['personId', 'contentId']).size().groupby('personId').size()
+print('# users: %d' % len(users_interactions_count_df))
+users_with_enough_interactions_df = users_interactions_count_df[users_interactions_count_df >= 5].reset_index()[['personId']]
+print('# users with at least 5 interactions: %d' % len(users_with_enough_interactions_df))
+# print(articles_df.head(5))
