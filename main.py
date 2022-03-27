f3a5e3c762fc09a2a6b174bcfa36cbaa937b6fd4
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-03-27 23:19:01 +1100

    popularity model update

--- main.py
+++ main.py
@@ -31,24 +31,24 @@ event_type_strength = {
 interactions_df['eventStrength'] = interactions_df['eventType'].apply(lambda x: event_type_strength[x])
 
 users_interactions_count_df = interactions_df.groupby(['personId', 'contentId']).size().groupby('personId').size()
-print('# users: %d' % len(users_interactions_count_df))
+# print('# users: %d' % len(users_interactions_count_df))
 users_with_enough_interactions_df = users_interactions_count_df[users_interactions_count_df >= 5].reset_index()[['personId']]
-print('# users with at least 5 interactions: %d' % len(users_with_enough_interactions_df))
+# print('# users with at least 5 interactions: %d' % len(users_with_enough_interactions_df))
 # print(articles_df.head(5))
 
-print('# of interactions: %d' % len(interactions_df))
+# print('# of interactions: %d' % len(interactions_df))
 interactions_from_selected_users_df = interactions_df.merge(users_with_enough_interactions_df,
                                                             how = 'right',
                                                             left_on = 'personId',
                                                             right_on = 'personId')
-print('# of interactions from users with at least 5 interactions %d' % len(interactions_from_selected_users_df))
+# print('# of interactions from users with at least 5 interactions %d' % len(interactions_from_selected_users_df))
 
 def smooth_user_preference(x):
     return math.log(1+x, 2)
 
 interactions_full_df = interactions_from_selected_users_df.groupby(['personId', 'contentId'])['eventStrength'].sum().apply(smooth_user_preference).reset_index()
 
-print('# of unique user/item interactions: %d' % len(interactions_full_df))
+# print('# of unique user/item interactions: %d' % len(interactions_full_df))
 interactions_full_df.head(10)
 
 interactions_train_df, interactions_test_df = train_test_split(interactions_full_df,
@@ -56,6 +56,151 @@ interactions_train_df, interactions_test_df = train_test_split(interactions_full
                                                                test_size=0.20,
                                                                random_state=42)
 
-print('# interactions on Train set: %d' % len(interactions_train_df))
-print('# interactions on Test set: %d' % len(interactions_test_df))
+# print('# interactions on Train set: %d' % len(interactions_train_df))
+# print('# interactions on Test set: %d' % len(interactions_test_df))
+
+interactions_full_indexed_df = interactions_full_df.set_index('personId')
+interactions_train_indexed_df = interactions_train_df.set_index('personId')
+interactions_test_indexed_df = interactions_test_df.set_index('personId')
+
+def get_items_interacted(person_id, interactions_df):
+    interacted_items = interactions_df.loc[person_id]['contentId']
+
+    return set(interacted_items if type(interacted_items) == pd.Series else [interacted_items])
+
+
+EVAL_RANDOM_SAMPLE_NON_INTERACTED_ITEMS = 100
+
+class ModelEvaluator:
+
+    def get_not_interacted_items_sample(self, person_id, sample_size, seed=42):
+        interacted_items = get_items_interacted(person_id, interactions_full_indexed_df)
+        all_items = set(articles_df['contentId'])
+        non_interacted_items = all_items - interacted_items
+
+        random.seed(seed)
+        non_interacted_items_sample = random.sample(non_interacted_items, sample_size)
+        return set(non_interacted_items_sample)
+
+    def _verify_hit_top_n(self, item_id, recommended_items, topn):
+        try:
+            index = next(i for i, c in enumerate(recommended_items) if c == item_id)
+        except:
+            index = -1
+        hit = int(index in range(0, topn))
+        return hit, index
+
+    def evaluate_model_for_user(self, model, person_id):
+        interacted_values_testset = interactions_test_indexed_df.loc[person_id]
+        if type(interacted_values_testset['contentId']) == pd.Series:
+            person_interacted_items_testset = set(interacted_values_testset['contentId'])
+        else:
+            person_interacted_items_testset = set([int(interacted_values_testset['contentId'])])
+        interacted_items_count_testset = len(person_interacted_items_testset)
+
+        person_recs_df = model.recommend_items(person_id, items_to_ignore=get_items_interacted(person_id, interactions_train_indexed_df), topn=10000000000)
+
+        hits_at_5_count = 0
+        hits_at_10_count = 0
+
+        for item_id in person_interacted_items_testset:
+            non_interacted_items_sample = self.get_not_interacted_items_sample(person_id,
+                                                                               sample_size=EVAL_RANDOM_SAMPLE_NON_INTERACTED_ITEMS,
+                                                                               seed=item_id % (2 ** 32))
+
+            items_to_filter_recs = non_interacted_items_sample.union(set([item_id]))
+
+            valid_recs_df = person_recs_df[person_recs_df['contentId'].isin(items_to_filter_recs)]
+            valid_recs = valid_recs_df['contentId'].values
+            hit_at_5, index_at_5 = self._verify_hit_top_n(item_id, valid_recs, 5)
+            hits_at_5_count += hit_at_5
+            hit_at_10, index_at_10 = self._verify_hit_top_n(item_id, valid_recs, 10)
+            hits_at_10_count += hit_at_10
+
+            recall_at_5 = hits_at_5_count / float(interacted_items_count_testset)
+            recall_at_10 = hits_at_10_count / float(interacted_items_count_testset)
+
+            person_metrics = {'hits@5_count': hits_at_5_count,
+                              'hits@10_count': hits_at_10_count,
+                              'interacted_count': interacted_items_count_testset,
+                              'recall@5': recall_at_5,
+                              'recall@10': recall_at_10}
+            return person_metrics
+
+    def evaluate_model(self, model):
+        # print('Running evaluation for users')
+        people_metrics = []
+        for idx, person_id in enumerate(list(interactions_test_indexed_df.index.unique().values)):
+            # if idx % 100 == 0 and idx > 0:
+            #    print('%d users processed' % idx)
+            person_metrics = self.evaluate_model_for_user(model, person_id)
+            person_metrics['_person_id'] = person_id
+            people_metrics.append(person_metrics)
+        print('%d users processed' % idx)
+
+        detailed_results_df = pd.DataFrame(people_metrics) \
+            .sort_values('interacted_count', ascending=False)
+
+        global_recall_at_5 = detailed_results_df['hits@5_count'].sum() / float(
+            detailed_results_df['interacted_count'].sum())
+        global_recall_at_10 = detailed_results_df['hits@10_count'].sum() / float(
+            detailed_results_df['interacted_count'].sum())
+
+        global_metrics = {'modelName': model.get_model_name(),
+                          'recall@5': global_recall_at_5,
+                          'recall@10': global_recall_at_10}
+        return global_metrics, detailed_results_df
+
+model_evaluator = ModelEvaluator()
+
+
+item_popularity_df = interactions_full_df.groupby('contentId')['eventStrength'].sum().sort_values(ascending=False).reset_index()
+item_popularity_df.head(10)
+
+class PopularityRecommender:
+
+    MODEL_NAME = 'Popularity'
+
+    def __init__(self, popularity_df, items_df=None):
+        self.popularity_df = popularity_df
+        self.items_df = items_df
+
+    def get_model_name(self):
+        return self.MODEL_NAME
+
+    def recommend_items(self, user_id, items_to_ignore=[], topn=10, verbose=False):
+        recommendations_df = self.popularity_df[~self.popularity_df['contentId'].isin(items_to_ignore)] \
+            .sort_values('eventStrength', ascending=False) \
+            .head(topn)
+
+        if verbose:
+            if self.items_df is None:
+                raise Exception('"items_df" is required in verbose mode')
+
+            recommendations_df = recommendations_df.merge(self.items_df, how='left',
+                                                          left_on='contentId',
+                                                          right_on='contentId')[
+                ['eventStrength', 'contentId', 'title', 'url', 'lang']]
+
+        return recommendations_df
+
+popularity_model = PopularityRecommender(item_popularity_df, articles_df)
+
+print('Evaluating Popularity recommendation model...')
+pop_global_metrics, pop_detailed_results_df = model_evaluator.evaluate_model(popularity_model)
+print('\nGlobal metrics:\n%s' % pop_global_metrics)
+pop_detailed_results_df.head(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
